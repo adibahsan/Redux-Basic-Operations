9ea7266ccf05016bf9ef6768cf9211dc53de52a2
Adib Ahsan Chowdhury <adibahsan@iut-dhaka.edu>
2020-08-22 22:33:27 +0600

    store status changed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
-import {store} from "./store/bugs";
+import {store,createBug,updateBug,deleteBug} from "./store/bugs";
 
 console.log("Hello World!");
 
-console.log(store );
+const unsubscribe = store.subscribe(
+    () =>
+    console.log("Redux Operation Executed", store.getState())
+)
+store.dispatch(
+    createBug("Bug1")
+)
+
+store.dispatch(
+    createBug("Bug2")
+)
+
+store.dispatch(
+    createBug("Bug3")
+)
+
+store.dispatch(
+    updateBug(3)
+)
+
+store.dispatch(
+    deleteBug(4)
+)
+
+console.log("state", store.getState() );
--- src/store/bugs.js
+++ src/store/bugs.js
@@ -5,13 +5,42 @@ const BUGS_ADDED = "createBugs";
 const BUGS_REMOVED = "removeBugs";
 const BUGS_RESOLVED = "resolvedBugs";
 
+//Action Creators
+
+export const createBug = description =>(
+    {
+        type : BUGS_ADDED,
+        payload : {
+            description
+        }
+    }
+)
+
+export const updateBug = id =>(
+    {
+        type : BUGS_RESOLVED,
+        payload : {
+            id
+        }
+    }
+)
+
+export const deleteBug = id =>(
+    {
+        type : BUGS_REMOVED,
+        payload : {
+           id
+        }
+    }
+)
+
 let lastId = 1;
 
 
 //Reducers
-export function bugReducers(state, action) {
+export function bugReducers(state =[], action) {
     if(action.type===BUGS_ADDED){
-        return[
+        return [
             ...state,
             {
                 id:++lastId,
@@ -21,12 +50,12 @@ export function bugReducers(state, action) {
         ];
     }
 
-    else if (action.type==BUGS_REMOVED){
-        return state.filter(bugs => bugs.id != action.payload.id);
+    else if (action.type===BUGS_REMOVED){
+        return state.filter(bugs => bugs.id !== action.payload.id);
     }
 
-    else if (action.type==BUGS_RESOLVED) {
-        return state.map(bugs => bugs.id != action.payload.id ? bugs : {...bugs, resolved: true});
+    else if (action.type===BUGS_RESOLVED) {
+        return state.map(bugs => bugs.id !== action.payload.id ? bugs : {...bugs, resolved: true});
     }
     else return state;
 }
