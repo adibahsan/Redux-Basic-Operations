eeb237544948aabb48427ecade6e772108b09966
Adib Ahsan Chowdhury <adibahsan@iut-dhaka.edu>
2020-08-22 23:02:38 +0600

    redux tool-kit store Create Store

--- src/store/bugStoreCreator.js
+++ src/store/bugStoreCreator.js
@@ -1,8 +1,11 @@
 import {createStore} from "redux";
+import {configureStore} from "@reduxjs/toolkit"
 import {bugReducers} from "./bugs";
 
 
 export function bugStoreCreator() {
-    return createStore(bugReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+    return configureStore({
+        reducer : bugReducers
+    });
 }
 
